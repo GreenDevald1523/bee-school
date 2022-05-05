fda53984ff5ce9e5fae6952e57bca618d66fcf11
GreenDevald1523 <46032258+GreenDevald1523@users.noreply.github.com>
2022-05-05 16:06:53 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+    $('.sec-1-teachers-mob').slick({
+        dots: true,
+        fade: true,
+        fadeSpeed: 1000,
+        arrows: false,
+        infinite: true,
+    })
+})
